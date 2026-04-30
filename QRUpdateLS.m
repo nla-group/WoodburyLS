6c49a0f57a0055da0ba094895fcd571fc168325c
Marcus Webb <marcusdavidwebb@gmail.com>
2026-04-30 11:10:52 +0100

    Add QRUpdateLS.m

---
+++ QRUpdateLS.m
@@ -0,0 +1,33 @@
+function x = QRUpdateLS(Q,R,U,V,b)
+% QRUpdateLS Economy QR update to least squares solution .
+% x = QRUpdateLS (Q,R,U,V,b) solves the least squares problem
+% min_x || ( QR + UV ' )x - b ||, where Q is m x n orthogonal ,
+% m >= n, R is n x n upper triangular , U is m x r, and V is n x r.
+[n,r] = size(V); U0 = U;
+% orthogonalise U wrt Q0
+U = U-Q*(Q'*U);
+[Qappend, ~] = qr(U ,0);
+QTb = [Q'*b;Qappend'*b];
+R = [R;zeros(r,n)];
+W = [Q, Qappend ]'*U0;
+% reduce W to upper triangular using Givens rotations, and apply to R and QTb
+for i = 1:r
+    for k = n -1+i: -1:0+i
+        [c,s] = Givens(W(k,i),W(k+1,i));
+        G = [c s;-s c];
+        idx = [k,k+1];
+        R(idx ,1:n) = G'*R(idx ,1:n);
+        W(idx ,:) = G'*W(idx ,:);
+        QTb(idx) = G'* QTb(idx);
+    end
+end
+R= R + W*V'; % R now has r subdiagonals
+% now return R to upper triangular form using Householder reflectors
+for i = 1:n
+    [v,beta] = House(R(i:i+r,i));
+    QTb(i:i+r) = QTb(i:i+r) - beta*v*(v'* QTb(i:i+r));
+    R(i:i+r,i:n) = R(i:i+r,i:n) - (beta*v)*(v'*R(i:i+r,i:n));
+end
+R = R(1:n ,1:n);
+x = R\QTb(1:n);
+end
