aae30d9468e7e1588e7d9a9de23580dbcd50aba3
Marcus Webb <marcusdavidwebb@gmail.com>
2026-05-07 16:22:06 +0100

    Fixed QRUpdateLS bug and improved demo.m

--- QRUpdateLS.m
+++ QRUpdateLS.m
@@ -5,15 +5,15 @@ function x = QRUpdateLS(Q,R,U,V,b)
 % m >= n, R is n x n upper triangular , U is m x r, and V is n x r.
 [n,r] = size(V);
 % orthogonalise U wrt Q0
-U1 = U-Q*(Q'*U);
+U1 = U-Q*(Q'*U); 
+U1 = U1-Q*(Q'*U1); % perform orthogonalisation twice
 [Qappend, ~] = qr(U1 ,0);
 QTb = [Q'*b; Qappend'*b];
 R = [R;zeros(r,n)];
 W = [Q, Qappend]'*U;
-disp(W)
 % reduce W to upper triangular using Givens rotations, and apply to R and QTb
-for i = r:-1:1
-    for k = n-1+2*i:-1:i
+for i = 1:r
+    for k = n-1+r:-1:0+i
         idx = [k,k+1];
         G = planerot(W(idx,i));
         R(idx,1:n) = G*R(idx,1:n);
@@ -21,9 +21,7 @@ for i = r:-1:1
         QTb(idx) = G*QTb(idx);
     end
 end
-R = R + W*V'; % R now has r subdiagonals
-disp(W)
-disp(R)
+R = R + W*V'; % R(1:n+r,1:n) is now upper trapezoidal
 % now return R to upper triangular form using Householder reflectors
 for i = 1:n
     [v,beta] = gallery("house", R(i:i+r,i));
