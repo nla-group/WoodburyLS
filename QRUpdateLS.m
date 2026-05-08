57295008d9a658098c52862f7daac3f892aa8e75
Marcus Webb <marcusdavidwebb@gmail.com>
2026-05-08 09:33:48 +0100

    Tidied up QRUpdateLS

--- QRUpdateLS.m
+++ QRUpdateLS.m
@@ -3,31 +3,32 @@ function x = QRUpdateLS(Q,R,U,V,b)
 % x = QRUpdateLS (Q,R,U,V,b) solves the least squares problem
 % min_x || ( QR + UV ' )x - b ||, where Q is m x n orthogonal,
 % m >= n, R is n x n upper triangular , U is m x r, and V is n x r.
+
 [n,r] = size(V);
-% orthogonalise U wrt Q0
+% orthogonalise U wrt Q
 U1 = U-Q*(Q'*U); 
 U1 = U1-Q*(Q'*U1); % perform orthogonalisation twice
-[Qappend, ~] = qr(U1 ,0);
+[Qappend, ~] = qr(U1,0);
 QTb = [Q'*b; Qappend'*b];
-R = [R;zeros(r,n)];
+R = [R; zeros(r,n)];
 W = [Q, Qappend]'*U;
 % reduce W to upper triangular using Givens rotations, and apply to R and QTb
 for i = 1:r
-    for k = n-1+r:-1:i
+    for k = n-1+r:-1:i % for LS solve we only need to reduce first n+r rows
         idx = [k,k+1];
         G = planerot(W(idx,i));
-        R(idx,1:n) = G*R(idx,1:n);
+        R(idx,:) = G*R(idx,:);
         W(idx,:) = G*W(idx,:);
         QTb(idx) = G*QTb(idx);
     end
 end
-R = R + W*V'; % R(1:n+r,1:n) is now upper trapezoidal
+R = R + W*V'; % R(1:n+r,:) is now upper trapezoidal
 % now return R to upper triangular form using Householder reflectors
 for i = 1:n
     [v,beta] = gallery("house", R(i:i+r,i));
     QTb(i:i+r) = QTb(i:i+r) - beta*v*(v'*QTb(i:i+r));
     R(i:i+r,i:n) = R(i:i+r,i:n) - (beta*v)*(v'*R(i:i+r,i:n));
 end
-R = R(1:n ,1:n);
+R = R(1:n,1:n);
 x = R\QTb(1:n);
 end
